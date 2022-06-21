536b174a7047517b4111bb08b5229620a434dc43
NICK <2981696426@qq.com>
2022-06-21 17:46:56 +0800

    2022-06-21 E

---
+++ src/EscapeCharacter.java
@@ -0,0 +1,11 @@
+/**
+ * @Description : TODO
+ * @Author : Nick
+ * @Date : 2022/6/21 17:40
+ * @Version : 1.0
+ **/
+public class EscapeCharacter {
+    public static void main(String[] args){
+        System.out.println("Good good study, day day up!");
+    }
+}
